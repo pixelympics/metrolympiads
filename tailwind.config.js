171c011cdebb225f5d50de2575e4e1afbcc38a75
LucBUTGH <lucleveque14@outlook.fr>
2024-03-21 09:57:41 +0100

    feat: setup colors and first view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                text: '#0b0911',
+                background: '#f6f5fa',
+                primary: '#6955b4',
+                secondary: '#a295d4',
+                accent: '#8572cc'
+            }
+        }
     },
     plugins: [],
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}']
