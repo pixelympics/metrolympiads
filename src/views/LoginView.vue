3924c07c86e774c491a8ea170cba2e5edc381bc9
Julien Ait azzouzene <julien.azzo27@gmail.com>
2024-03-21 10:32:07 +0100

    💄 ui: login page style

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,23 @@
-<script setup>
-console.log('test');
-</script>
+<script setup></script>
 
 <template>
-    <div class="h-full background"></div>
+    <div class="flex flex-col items-center gap-2">
+        <h1 class="text-3xl m-5">METROLYMPIADES</h1>
+
+        <h2 class="text-2xl m-2">Login</h2>
+
+        <div class="border-2 border-black bg-secondary p-5">
+            <form @submit.prevent="onSubmit" class="flex flex-col items-center gap-2">
+                <label for="email">Email</label>
+                <input type="email" name="email" v-model="email" required />
+
+                <label for="password">Password</label>
+                <input type="password" name="password" v-model="password" required />
+
+                <button type="submit" class="bg-primary text-background p-2 rounded-md">
+                    Login
+                </button>
+            </form>
+        </div>
+    </div>
 </template>
