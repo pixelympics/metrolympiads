05754ad0c264757e71a024bce60bc6c446a2bf6a
LucBUTGH <lucleveque14@outlook.fr>
2024-03-24 20:44:45 +0100

    feat: create an account

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,16 +4,31 @@ import Header from '@/components/HeaderComponent.vue';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { supabase } from '@/lib/supabase.js';
+import AccountForm from '@/components/AccountForm.vue';
+import FormField from '@/components/FormField.vue';
 
 const email = ref('');
 const password = ref('');
 const confpassword = ref('');
+let emailExists = ref(false);
 
 const { push: routerPush } = useRouter();
 
+const onEmailChange = (value) => {
+    email.value = value;
+};
+
+const onPasswordChange = (value) => {
+    password.value = value;
+};
+
+const onConfirmPasswordChange = (value) => {
+    confpassword.value = value;
+};
+
 const onSubmit = async () => {
     if (
-        password.value !== confpassword.value ||
+        supabase.password.value !== confpassword.value ||
         !email.value ||
         !password.value ||
         !confpassword.value
@@ -29,7 +44,7 @@ const onSubmit = async () => {
     if (error) {
         alert(error.message);
     } else {
-        routerPush({ name: '' });
+        routerPush('/team-dashboard');
     }
     return data;
 };
@@ -37,52 +52,43 @@ const onSubmit = async () => {
 
 <template>
     <Header />
-    <div class="h-full bg-background">
-        <div class="flex justify-center items-center h-full">
-            <div class="p-8 rounded-lg shadow-lg">
-                <h1 class="text-2xl justify-center font-bold mb-4">Create an account</h1>
-                <form @submit.prevent="onSubmit">
-                    <div class="mb-4">
-                        <label for="email" class="block text-sm font-medium text-text">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
-                    </div>
-                    <div class="mb-4">
-                        <label for="password" class="block text-sm font-medium text-text"
-                            >Password</label
-                        >
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
-                    </div>
-                    <div class="mb-4">
-                        <label for="confpassword" class="block text-sm font-medium text-text"
-                            >Confirm password</label
-                        >
-                        <input
-                            type="password"
-                            id="confpassword"
-                            name="confpassword"
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        />
-                    </div>
-                    <div class="mb-4">
-                        <button
-                            type="submit"
-                            class="w-full bg-indigo-500 text-white font-semibold p-2 rounded-md"
-                        >
-                            Let's goooooooooo
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
+    <div class="m-5 flex flex-col items-center gap-2">
+        <h1 class="text-2xl justify-center font-bold mb-4">Create an account</h1>
+        <AccountForm
+            @submit="onSubmit"
+            id="signup-form"
+            submit-button-text="Sign up"
+            caption="Sign up"
+        >
+            <form-field
+                @update:value="onEmailChange"
+                id="email-input"
+                label="Email"
+                name="email"
+                type="email"
+                required
+                :model-value="email"
+            />
+
+            <form-field
+                @update:value="onPasswordChange"
+                id="password-input"
+                label="Password"
+                name="password"
+                type="password"
+                required
+                :model-value="password"
+            />
+
+            <form-field
+                @update:value="onConfirmPasswordChange"
+                id="confpassword-input"
+                label="Confirm password"
+                name="confpassword"
+                type="password"
+                required
+                :model-value="confpassword"
+            />
+        </AccountForm>
     </div>
 </template>
