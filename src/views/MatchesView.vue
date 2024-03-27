98320e51b062628ba112902dd10e2d6d8e20a174
SirSoliloque <victorfriboulet07@gmail.com>
2024-03-27 14:36:49 +0100

    :fix retrait d'un console log qui crash l'appli

--- src/views/MatchesView.vue
+++ src/views/MatchesView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue';
+import MatchItem from '@/components/MatchItem.vue';
 import { supabase } from '@/lib/supabase'
 import { onMounted, ref } from 'vue';
 import { useUserStore } from '@/stores/user'
@@ -25,7 +26,6 @@ onMounted(() => {
     getMatchList()
 })
 
-{{ console.log(user.value.id) }}
 
 </script>
 
@@ -34,17 +34,22 @@ onMounted(() => {
     <div class="flex flex-col items-center">
         <h1 class="text-3xl m-5">Welcome</h1>
 
-        
-        <div v-for="(match) in MatchList" class="p-4" :key="match">
+        <table  class="table-auto">
+            <thead>
+                    <tr>
+                        <th></th>
+                        <th class="px-4 py-2">Team 1</th>
+                        <th class="px-4 py-2">Team 2</th>
+                        <th class="px-4 py-2">Score</th>
+                    </tr>
+                </thead>
+            <tbody>
+                <div v-for="(match) in MatchList" class="p-4" :key="match">
 
-            
-            <!--
-                SLT VIC
-                match c'est l'element entier, si tu veux l'utiliser faut faire match.id ou match.score_team1 par exemple
-                jsp encore comment tu vas créer ton component donc je laisse ca ici
-            -->
-            
-        </div>
-
+                    <MatchItem match="match"/>
+                
+                </div>
+            </tbody>
+        </table>
     </div>
 </template>
