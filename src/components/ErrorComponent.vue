a6431be79dce61ad518cb9a13a5e8971f3468038
LucBUTGH <lucleveque14@outlook.fr>
2024-03-28 17:18:55 +0100

    feat: popup error toast

---
+++ src/components/ErrorComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <transition name="slide-fade">
+        <div
+            v-if="isVisible"
+            class="fixed top-0 inset-x-0 flex items-center justify-center px-4 py-2"
+        >
+            <div class="max-w-md p-6 bg-red-100 border border-red-400 text-red-700 rounded-md">
+                <slot></slot>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isVisible: false
+        };
+    },
+    mounted() {
+        this.isVisible = true;
+
+        setTimeout(() => {
+            this.isVisible = false;
+        }, 1500);
+    }
+};
+</script>
+
+<style>
+.slide-fade-enter-active {
+    transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+    transition: all 0.3s ease;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+    transform: translateY(-20px);
+    opacity: 0;
+}
+</style>
