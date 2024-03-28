2af655cb6b7f4da05f13a8f21a4182f7bced2b16
LucBUTGH <lucleveque14@outlook.fr>
2024-03-28 21:33:45 +0100

    feat: add heroicons

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,7 +2,6 @@
 import { ref, watch } from 'vue';
 import Header from '@/components/HeaderComponent.vue';
 import FormField from '@/components/FormField.vue';
-import TestIcon from '@/components/icons/IconTooling.vue';
 import { supabase } from '@/lib/supabase';
 import { useUserStore } from '@/stores/user';
 import { storeToRefs } from 'pinia';
@@ -114,10 +113,21 @@ const deleteMember = (id) => {
                 required
             />
             <button
-                class="bg-gray-500 text-white font-semibold p-2 rounded-md"
+                class="bg-gray-500 hover:bg-gray-700 text-white font-semibold p-2 mb-2 rounded-md"
                 @click="deleteMember(member.id)"
             >
-                <TestIcon />
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="w-6 h-6"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M2.515 10.674a1.875 1.875 0 0 0 0 2.652L8.89 19.7c.352.351.829.549 1.326.549H19.5a3 3 0 0 0 3-3V6.75a3 3 0 0 0-3-3h-9.284c-.497 0-.974.198-1.326.55l-6.375 6.374ZM12.53 9.22a.75.75 0 1 0-1.06 1.06L13.19 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L15.31 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
             </button>
         </div>
         <div class="mb-4" v-if="teamMembers.length < 5">
