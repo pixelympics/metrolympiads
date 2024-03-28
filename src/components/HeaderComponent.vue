5b062f95ad11b2390e2e785386bd2ecb6ac3bf75
Julien Ait azzouzene <julien.azzo27@gmail.com>
2024-03-28 11:24:43 +0100

    🚧 wip: add links to the header; working on navigation

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,33 +4,29 @@ import { useRouter } from 'vue-router';
 import { supabase } from '@/lib/supabase';
 import { useUserStore } from '@/stores/user';
 import { storeToRefs } from 'pinia';
-import { watch } from 'vue';
-import {ref} from 'vue';
+import { onMounted, watch } from 'vue';
+import { ref } from 'vue';
 
-const router = useRouter();
+const { push: routerPush } = useRouter();
 const { user } = storeToRefs(useUserStore());
-const props = defineProps({title: String});
+const props = defineProps({ title: String });
+
+const loggedIn = ref(!!user);
 
 watch(user, async (newUser) => {
-    if(props.title){
+    if (props.title) {
         return;
     }
 
-    if(newUser) {
-        headerTitle.value = 'Team ' + await fetchTeamName(newUser.id);
-    } else{
-        headerTitle.value = 'Guest';
-    }
+    updateConnectionStatus(newUser);
 });
-
-
 
 const headerTitle = ref('Guest');
 
 const loading = ref(false);
 //TODO only displays certain elements if user is logged in
 
-const logout = async function logout() {
+const logout = async function () {
     loading.value = true;
     const { error } = await supabase.auth.signOut();
 
@@ -38,8 +34,28 @@ const logout = async function logout() {
         console.error(error);
     }
 
-    router.push('/');
-}
+    routerPush('/');
+};
+
+const goToTeamDashboard = function () {
+    routerPush('/team-dashboard');
+};
+
+const goToRanking = function () {
+    routerPush('/ranking');
+};
+
+const goToMatchs = function () {
+    routerPush('/matchs');
+};
+
+const goToLogin = function () {
+    routerPush('/login');
+};
+
+const goToSignup = function () {
+    routerPush('/signup');
+};
 
 const fetchTeamName = async function fetchTeamName(teamId) {
     const { data, error } = await supabase
@@ -53,14 +69,33 @@ const fetchTeamName = async function fetchTeamName(teamId) {
     }
 
     return data.name;
-}
+};
 
+const updateConnectionStatus = async function (newUser) {
+    if (newUser) {
+        headerTitle.value = 'Team ' + (await fetchTeamName(newUser.id));
+        loggedIn.value = true;
+    } else {
+        headerTitle.value = 'Guest';
+        loggedIn.value = false;
+    }
+};
+
+onMounted(async () => {
+    await updateConnectionStatus(user.value);
+});
 </script>
 
 <template Header>
     <Slide>
-        <button v-on:click="router.push('/')">Home</button>
-        <button :loading="loading" @click="logout">
+        <button v-if="loggedIn" @click="goToTeamDashboard">Team settings</button>
+        <button @click="goToRanking">Ranking</button>
+        <button v-if="loggedIn" @click="goToMatchs">Matchs</button>
+
+        <button v-if="!loggedIn" @click="goToLogin">Login</button>
+        <button v-if="!loggedIn" @click="goToSignup">Sign Up</button>
+
+        <button v-if="loggedIn" :loading="loading" @click="logout">
             <span v-if="loading">...</span>
             <span v-else>Logout</span>
         </button>
