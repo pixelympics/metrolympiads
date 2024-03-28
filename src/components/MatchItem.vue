3a785ecc003dbe6342f766c8c63bcef9c4780527
Julien Ait azzouzene <julien.azzo27@gmail.com>
2024-03-28 21:15:42 +0100

    🐛fix: the matchs are displayed, but lack styling; navigation between match list and creation

--- src/components/MatchItem.vue
+++ src/components/MatchItem.vue
@@ -1,24 +1,23 @@
 <script setup>
-const props = defineProps(
-    {
-        match: {
-            type: Object,
-            required: true,
-        }
+const props = defineProps({
+    match: {
+        type: Object,
+        required: true
     }
-);
+});
 
+console.log(props.match);
 </script>
 
 <template>
     <table class="table-auto">
         <tbody>
-        <tr>
-            <td>{{ props.match.sport }}</td>
-            <td>{{ props.match.team1.name }}</td>
-            <td>{{ props.match.team2.name }}</td>
-            <td>{{ props.match.time }}</td>
-        </tr>
+            <tr>
+                <td>{{ match.sport }}</td>
+                <td>{{ match.team1.name }}</td>
+                <td>{{ match.team2.name }}</td>
+                <td>{{ match.time }}</td>
+            </tr>
         </tbody>
-    </table>    
+    </table>
 </template>
